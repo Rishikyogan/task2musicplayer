58b95e4abb580e3181228095866018a026b9b5c4
Rishikyogan <146551701+Rishikyogan@users.noreply.github.com>
2023-09-30 19:25:49 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,27 @@
+let allMusic = [
+    {
+      name: "Sorry",
+      artist: "Justin bieber",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "hayoda",
+      artist: "anirudh",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "collide",
+      artist: "Justine Skye",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "dandelions",
+      artist: "ruth",
+      img: "music-4",
+      src: "music-4"
+    },
+  
+  ];
